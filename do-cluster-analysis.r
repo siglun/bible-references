9aa42f57c7e8a68a56722909b051efaf14cc120a
Sigfrid Lundberg <siggelundberg@gmail.com>
2022-12-11 15:18:47 +0100

    the cladogram is readable

--- do-cluster-analysis.r
+++ do-cluster-analysis.r
@@ -2,11 +2,21 @@
 
 
 yearly_quotes <- read.table(file = "clustering-data.text",
+	      	     row.names=1,
                      sep = "",
                      quote = "\"",
                      dec = ".",
                      fill = TRUE,
                      comment.char = "#")
 
+# yearly_quotes
 
-yearly_quotes
+
+
+pdf( "cladogram.pdf", width = 20, height = 20 )
+
+d=dist(yearly_quotes,method="euclidean")
+
+hc=hclust(d,method="complete")
+
+plot(hc)
