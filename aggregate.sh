edc661aaae5034521c970c64e6c8527bbefae224
Sigfrid Lundberg <siggelundberg@gmail.com>
2023-01-08 11:09:11 +0100

    chosing the right references

--- aggregate.sh
+++ aggregate.sh
@@ -7,43 +7,40 @@ source "parameters.sh"
 #
 
 $SAXON table-all.xml aggregate-per-year.xsl > aggregated-references-per-year.text
-
-$SAXON table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_1.text
-$SAXON ref='1 Kor 13,12'   table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_2.text
-$SAXON ref='1 Kor 13,13'   table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_3.text
-$SAXON ref='Matt 28,18-20' table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_4.text
-$SAXON ref='Sl 23,4' table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_5.text
+$SAXON ref='Sl 23,4'    table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_1.text
+$SAXON ref='Ordsp 4,23' table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_2.text
+$SAXON ref='Joh 6,63'   table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_3.text
+$SAXON ref='Matt 16,18' table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_4.text
+$SAXON ref='1 Mos 2,7'  table-all.xml aggregate-per-year-for-given-reference.xsl > selected_ref_5.text
 
 #
 # The poetry and the verses
 #
 
 $SAXON table-poetry.xml aggregate-per-year.xsl > aggregated-poetry-references-per-year.text
-
-$SAXON table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_1.text
-$SAXON ref='1 Mos 2,7'   table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_2.text
+$SAXON ref='Sl 23,4'    table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_1.text
+$SAXON ref='Ordsp 4,23' table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_2.text
 $SAXON ref='Joh 6,63'   table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_3.text
-$SAXON ref='Ordsp 4,23' table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_4.text
-$SAXON ref='Sl 23,4' table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_5.text
+$SAXON ref='Matt 16,18' table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_4.text
+$SAXON ref='1 Mos 2,7'  table-poetry.xml aggregate-per-year-for-given-reference.xsl > selected_poetry_ref_5.text  
 
 #
 # and, finally, the prose
 #
 
 $SAXON table-prose.xml aggregate-per-year.xsl > aggregated-prose-references-per-year.text
-
-$SAXON table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_1.text
-$SAXON ref='1 Mos 2,7'   table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_2.text
+$SAXON ref='Sl 23,4'    table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_1.text
+$SAXON ref='Ordsp 4,23' table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_2.text
 $SAXON ref='Joh 6,63'   table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_3.text
-$SAXON ref='Ordsp 4,23' table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_4.text
-$SAXON ref='Sl 23,4' table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_5.text
+$SAXON ref='Matt 16,18' table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_4.text
+$SAXON ref='1 Mos 2,7'  table-prose.xml aggregate-per-year-for-given-reference.xsl > selected_prose_ref_5.text  
 
 #
 # the distributions
 #
 
 xpath -q -e '//tr/td[2]/text()' table-all.xml     | sort |uniq -c | sort -n > sorted_uniq_keys.text
-xpath -q -e '//tr/td[2]/text()' table-poetry.xml  | sort |uniq -c | sort -n > sorted_uniq_keys_for_poetry.text
+xpath -q -e '//tr/td[2]/text()' table-prose.xml  | sort |uniq -c | sort -n > sorted_uniq_keys_for_prose.text
 xpath -q -e '//tr/td[2]/text()' table-prose.xml   | sort |uniq -c | sort -n > sorted_uniq_keys_for_prose.text
 
 #
@@ -53,7 +50,6 @@ xpath -q -e '//tr/td[2]/text()' table-prose.xml   | sort |uniq -c | sort -n > so
 perl -ne 's/^\s*//;s/\s.*$//;print;' < sorted_uniq_keys.text             | uniq -c > distribution.text
 perl -ne 's/^\s*//;s/\s.*$//;print;' < sorted_uniq_keys_for_poetry.text  | uniq -c > distribution_poetry.text
 perl -ne 's/^\s*//;s/\s.*$//;print;' < sorted_uniq_keys_for_prose.text   | uniq -c > distribution_prose.text
-
 
 #
 # then we plot
---
+++ plot_selected_prose_references.gp
@@ -0,0 +1,31 @@
+#
+# plot grand totals
+#
+set xlabel "Year"
+set ylabel "Number of references"
+
+
+set ter eps
+set out "selected_poetry_refs_per_year.eps"
+set monochrome 
+# set key left top
+
+plot 'selected_poetry_ref_1.text' using 1:2 w l t "Matt 16,18", \
+     'selected_poetry_ref_2.text' using 1:2 w l t "1 Mos 2,7", \
+     'selected_poetry_ref_3.text' using 1:2 w l t "Joh 6,63", \
+     'selected_poetry_ref_4.text' using 1:2 w l t "Ordsp 4,23", \
+     'selected_poetry_ref_5.text' using 1:2 w l t "Sl 23,4" 
+
+
+# set ter pdfcairo
+# set out "selected_poetry_refs_per_year.pdf"
+# set monochrome 
+# replot
+
+
+set ter png
+set out "selected_poetry_refs_per_year.png"
+set monochrome 
+
+replot
+
