1a60fc59cb0215f8d2fa25904282bfb0829a2c4c
Sigfrid Lundberg <siggelundberg@gmail.com>
2023-01-09 12:05:53 +0100

    cladograms for poetry and prose

---
+++ cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source "parameters.sh"
+
+$SAXON table-prose.xml extract_clustering_data.xsl  > clustering-data-prose.text
+$SAXON table-poetry.xml extract_clustering_data.xsl  > clustering-data-poetry.text
+
+
+
+./do-cluster-analysis.r --f clustering-data-prose.text --out cladogram-prose.pdf
+./do-cluster-analysis.r --f clustering-data-poetry.text --out cladogram-poetry.pdf
